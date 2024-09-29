19d6c79a905531fde083dd226dcf1b312c0d00f1
JaoSchmidt <jhsc98@gmail.com>
2024-09-28 22:53:26 -0300

    Adding freetype and freetype-gl externally
    The search for making stuff easier to build begins.
    Motive: Need to find a way to render text without rewritting the entire freetype-gl repo, the fastest way atm seems to add it externally and begin to use their resources little by little

--- Pain/src/CoreFiles/ResourceManagerSing.cpp
+++ Pain/src/CoreFiles/ResourceManagerSing.cpp
@@ -1,7 +1,9 @@
-﻿#include "CoreFiles/ResourceManagerSing.h"
+#include "CoreFiles/ResourceManagerSing.h"
+
 #include <SDL2/SDL_surface.h>
 
-#include <SDL_image.h>
+// #include <SDL_image.h>
+#include "external/SDL_image/include/SDL_image.h"
 namespace pain
 {
 
